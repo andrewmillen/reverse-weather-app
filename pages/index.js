19cb0857198f66a9001dffd7a9a2d76011637979
andrewmillen <andrewmillen1@gmail.com>
2023-01-28 16:41:54 -0600

    Proof of concept

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,51 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
 
 const citiesDataSource =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
-const forecastDataBase = "https://api.open-meteo.com/v1/forecast";
+const forecastDataBaseUrl = "https://api.open-meteo.com/v1/forecast";
 
-async function myFunction() {
+async function findMatchingCities(temp) {
+  // Get list of cities
   const citiesData = await (await fetch(citiesDataSource)).json();
 
+  // Create array for matching cities to go into
+  const matchingCities = [];
+
+  // For each city, send an API call to the weather data source
   for (var i = 0; i < 10; i++) {
     const cityName = citiesData[i].city;
     const lat = citiesData[i].latitude;
     const lon = citiesData[i].longitude;
 
-    const weatherDataSource = `${forecastDataBase}?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    const weatherDataSource = `${forecastDataBaseUrl}?latitude=${lat}&longitude=${lon}&current_weather=true`;
     const weatherData = await (await fetch(weatherDataSource)).json();
 
-    console.log(cityName, weatherData);
+    console.log(weatherData);
 
-    // if (weatherData.current_weather.temperature === inputTemp ) {
-    //   var matchingCity = document.createElement("li")
-    //   matchingCity.innerHTML = info[i].name
-    //   result.appendChild(matchingCity)
-    // } else {
-    //   console.log(info[i].name, 'not a match')
-    // }
+    // Then check if the given temp matches the temp of the city
+    // If it does, add it to the array
+    weatherData.elevation == temp && matchingCities.push(cityName);
   }
+
+  // Return the result
+  return matchingCities;
 }
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [resultsContent, setResultsContent] = useState([]);
+  const [hasMatches, setHasMatches] = useState(true);
+
+  async function handleOnClick(temp) {
+    setIsLoading(true);
+
+    const results = await findMatchingCities(temp);
+    results.length ? setResultsContent(results) : setHasMatches(false);
+
+    setIsLoading(false);
+  }
+
   return (
     <>
       <Head>
@@ -41,7 +59,17 @@ export default function Home() {
       </Head>
       <main>
         <h1>Where is it 72° and sunny right now?</h1>
-        <button onClick={myFunction}>Tell me</button>
+        <button onClick={() => handleOnClick(32)}>Tell me</button>
+        <br />
+        {isLoading && "Loading"}
+        <br />
+        <ul>
+          {hasMatches
+            ? resultsContent.map((result, index) => (
+                <li key={index}>{result}</li>
+              ))
+            : "There were no matching cities"}
+        </ul>
       </main>
     </>
   );
