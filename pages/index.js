7ebd1ad7451fe6fd3d2ba8f61d6983985c1e1007
andrewmillen <andrewmillen1@gmail.com>
2023-01-28 10:41:45 -0600

    Pull in and loop through data sources

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
 import Head from "next/head";
 
+const citiesDataSource =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+const forecastDataBase = "https://api.open-meteo.com/v1/forecast";
+
+async function myFunction() {
+  const citiesData = await (await fetch(citiesDataSource)).json();
+
+  for (var i = 0; i < 10; i++) {
+    const cityName = citiesData[i].city;
+    const lat = citiesData[i].latitude;
+    const lon = citiesData[i].longitude;
+
+    const weatherDataSource = `${forecastDataBase}?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    const weatherData = await (await fetch(weatherDataSource)).json();
+
+    console.log(cityName, weatherData);
+
+    // if (weatherData.current_weather.temperature === inputTemp ) {
+    //   var matchingCity = document.createElement("li")
+    //   matchingCity.innerHTML = info[i].name
+    //   result.appendChild(matchingCity)
+    // } else {
+    //   console.log(info[i].name, 'not a match')
+    // }
+  }
+}
+
 export default function Home() {
   return (
     <>
@@ -14,6 +41,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>Where is it 72° and sunny right now?</h1>
+        <button onClick={myFunction}>Tell me</button>
       </main>
     </>
   );
